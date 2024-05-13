b478e82fb1bd6413722f08ecbe545d723f11c743
garciaroi <garciaroi@esat-miralles.com>
2024-05-13 13:17:49 +0200

    delete build files

--- EDK3_Login/build/PR_Demo_Workspace/vs2019/shaders/basicFragment.fs
+++ EDK3_Login/build/PR_Demo_Workspace/vs2019/shaders/basicFragment.fs
@@ -8,5 +8,10 @@ out vec4 fragColor;
 in vec2 uv;
 in vec3 normal;
 void main() {
+   if(0 == u_use_texture){
+   //  fragColor = u_color;
    fragColor = vec4(normal,1.0);
+   }else{
+    fragColor = texture(u_albedo_2,uv);
+   }
 }
